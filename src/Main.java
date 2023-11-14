6ad9bf73dbf2e3ea621450442b1c9f88f40f4a50
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-14 15:33:56 +0300

    Palindrome function completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number");
+        while (isNumber(scn)) {
+            int number = scn.nextInt();
+            if (isPalindrome(number)) {
+                System.out.println(number + " is a palindrome number");
+            } else {
+                System.out.println(number + " is not a palindrome number");
+            }
+        }
+    }
+
+    /**
+     * finder palindrome func!!
+     * @param num given num input
+     * @return true-false
+     */
+    public static boolean isPalindrome(int num) {
+        if (num < 0) {
+            num = Math.abs(num);
+        }
+        String strNum = Integer.toString(num);
+        StringBuilder nStr = new StringBuilder();
+        char ch;
+        for (int i = 0; i < strNum.length(); i++) {
+            ch = strNum.charAt(i);
+            nStr.insert(0, ch);
+        }
+        return nStr.toString().equals(strNum);
+    }
+
+    /**
+     * is input a number ? function
+     * @param scn input value
+     * @return true-false
+     */
+    public static boolean isNumber(Scanner scn) {
+        if (scn.hasNextInt()) {
+            return true;
+        } else {
+            System.out.println("Invalid entry. This is not a number.");
+        }
+        return false;
     }
 }
